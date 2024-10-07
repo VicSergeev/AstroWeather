c00ac8478f2ed3d73c93dc0b440b75ab11a1a213
That Guy Vic <vicsergeevrov@icloud.com>
2024-10-07 15:38:00 +0300

    view controller added programatically

---
+++ AstroWeather/ViewControllers/MainViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  AstroWeather
+//
+//  Created by Victor on 07.10.2024.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+    
+    // MARK: - UI elements
+    
+    // MARK: - Methods
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
+// MARK: - Extensions
+
